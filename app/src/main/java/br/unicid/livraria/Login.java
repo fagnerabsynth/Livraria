2a2fe4149af75e71f6fdf5ffa74f9edb30a9ea10
Renan <j.renan.r@gmail.com>
2015-11-17 23:08:19 -0200

    As páginas 1, 2 e 3. Somente os Xml foram criados.

---
+++ app/src/main/java/br/unicid/livraria/Login.java
@@ -0,0 +1,18 @@
+package br.unicid.livraria;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+
+}
