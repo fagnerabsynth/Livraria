37d9e3383c7d794981924afa63dc6bad87e74527
Fagner <tagnisp@hotmail.com>
2015-11-23 18:16:01 -0200

    Banco de dados SQLITE

--- app/src/main/java/br/unicid/livraria/Data/DataBase.java
+++ app/src/main/java/br/unicid/livraria/Data/DataBase.java
@@ -1,8 +1,12 @@
 package br.unicid.livraria.Data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by Fagner on 11/11/2015.
@@ -12,16 +16,50 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DataBase extends SQLiteOpenHelper {
 
 
-    public DataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String DB = "bancodedados.db";
+    private static final int VERSAO = 1;
+    private String TABELA_PRODUTOS = "produtos", TABELA_CATEGORIA = "categoria", TABELA_USUARIOS = "login", TABELA_ALUNOS = "alunos";
+    private SQLiteDatabase db;
+
+
+    public DataBase(Context context) {
+        super(context, DB, null, VERSAO);
     }
 
-    public void DataBase() {
+    //Trabalha com MD5 para comparaçao - diferenciando maiusculas e minusculas para a senha
+    public static String md5(String s) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
-
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+
+
+        //Cria tabela dos usuarios
+        String query1 = "Create table " + TABELA_USUARIOS + " (id integer primary key autoincrement, usuario text not null, senha text not null )";
+        db.execSQL(query1);
+
+        //adiciona o usuario admin e a senha admin
+        Cursor rs = db.rawQuery("SELECT * FROM " + TABELA_USUARIOS, null);
+        if (rs.getCount() == 0) {
+            String[] dadosAdmin = {"admin", md5("admin")};
+            db.execSQL("Insert into " + TABELA_USUARIOS + "(email,senha) values (?,?)", dadosAdmin);
+        }
+
+
+
+
 
     }
 
@@ -29,6 +67,14 @@ public class DataBase extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+    //verifica o login do usuario
+    public boolean login(String usuario, String senha) {
+        db = this.getWritableDatabase();
+
+        return false;
+    }
+
 
 
 }
