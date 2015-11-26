396fec9cbdecc5a39066ea6d20b59f69ae48d9b6
Renan <j.renan.r@gmail.com>
2015-11-26 17:11:17 -0200

    paginas 1,2,3 e 4
    faltam as Classes 'Catalogo' e 'Sobre'

--- app/src/main/java/br/unicid/livraria/Principal.java
+++ app/src/main/java/br/unicid/livraria/Principal.java
@@ -1,15 +1,61 @@
 package br.unicid.livraria;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class Principal extends AppCompatActivity {
+
+public class Principal extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
     }
+
+
+    public void catalogoLivros(View btn) {
+        Intent pulo = new Intent(this,Catalogo.class);
+
+        startActivity(pulo);
+
+
+    }
+
+    public void administrar(View btn){
+        Intent pulo = new Intent(this,Administracao.class);
+
+        startActivity(pulo);
+
+
+    }
+
+    public void sobreAplicacao(View btn) {
+
+        Intent pulo = new Intent(this,Sobre.class);
+
+        startActivity(pulo);
+    }
+
+    public void finalizarSessao(View btn){
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("sessao", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.remove("sessao");
+        editor.commit();
+
+        Intent pulo = new Intent(this,Inicial.class);
+
+        startActivity(pulo);
+        finish();
+
+
+    }
+
+
+
 
 
 }
