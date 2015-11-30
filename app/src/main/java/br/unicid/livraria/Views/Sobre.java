daa621054127b4f6fa47923e45fde1cc07a492c9
fagnerabsynth <tagnisp@hotmail.com>
2015-11-30 15:03:22 -0200

    Mostrar dados do usuario ok Edição de dados do usuario ok

--- app/src/main/java/br/unicid/livraria/Views/Sobre.java
+++ app/src/main/java/br/unicid/livraria/Views/Sobre.java
@@ -1,10 +1,13 @@
 package br.unicid.livraria.Views;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import br.unicid.livraria.Inicial;
 import br.unicid.livraria.R;
 
 public class Sobre extends AppCompatActivity {
@@ -15,25 +18,36 @@ public class Sobre extends AppCompatActivity {
         setContentView(R.layout.sobre);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sobre, menu);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(Inicial.Cor())));
+        getSupportActionBar().setTitle(Inicial.TITULO());
+        getSupportActionBar().setSubtitle("Sobre o APP");
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+
+                int id = item.getItemId();
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
